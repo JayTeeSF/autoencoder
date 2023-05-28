ae5b2de6cda30e1115442cdbcaf6479a9ab2f42b
JayTeeSF <jaytee@jayteesf.com>
2023-05-28 11:35:59 -0700

    use tokenizer for prediction input

--- autoencoder.py
+++ autoencoder.py
@@ -3,8 +3,7 @@ import tensorflow as tf
 import argparse
 
 # Parse command line arguments
-parser = argparse.ArgumentParser(description='Autoencoder Training and Prediction')
-parser.add_argument('--train', action='store_true', help='Train and save the model')
+parser = argparse.ArgumentParser(description='Autoencoder Loading and Prediction')
 parser.add_argument('--load', type=str, help='Load a saved model from the specified path')
 parser.add_argument('--predict', nargs='+', help='Make predictions using the loaded model on the given input')
 args = parser.parse_args()
@@ -12,19 +11,18 @@ args = parser.parse_args()
 if args.load:
     # Load a saved model
     model_path = args.load
+    print('Loading model: ', model_path,'...')
     autoencoder = tf.keras.models.load_model(model_path)
     print('Model loaded successfully.')
 
 if args.predict:
-    if tokenizer is None:
-        print('Error: No tokenizer found. Please specify --train to train a model or --load to load a trained model.')
-    else:
-        # Tokenize and pad the input sequence
-        input_sequence = args.predict
-        input_data = tokenizer.texts_to_sequences(input_sequence)
-        input_data = tf.keras.preprocessing.sequence.pad_sequences(input_data, maxlen=input_dim)
+    tokenizer = tf.keras.preprocessing.text.Tokenizer()
+    # Tokenize and pad the input sequence
+    input_sequence = args.predict
+    input_data = tokenizer.texts_to_sequences(input_sequence)
+    input_data = tf.keras.preprocessing.sequence.pad_sequences(input_data, maxlen=input_dim)
 
-        # Make predictions using the loaded model
-        output_data = autoencoder.predict(input_data)
-        print('Input:', input_data)
-        print('Output:', output_data)
+    # Make predictions using the loaded model
+    output_data = autoencoder.predict(input_data)
+    print('Input:', input_data)
+    print('Output:', output_data)
